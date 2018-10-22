4caa2c3325cdb3343a34c449ead5e8a7a7984313
hackingyseguridad <hackingyseguridad@hackingyseguridad.com>
2018-10-22 23:07:17 +0200

    Update curl.sh

--- curl.sh
+++ curl.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+# Ejemplo de peticion web con curl completa y bien formada
+#
 curl --insecure --silent --compressed \
 -H "User-Agent: Mozilla/5.0 (Windows NT 6.1; Win64; x64; rv:62.0) Gecko/20100101 Firefox/62.0" \
 -H "Accept: text/html, applicattion/xhtml+xml, application/xml;q=0.9,*/*;q=0.8" \
