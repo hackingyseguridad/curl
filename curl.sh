10c7303e3a3851cdf2ca56fde7737687c1daec44
hackingyseguridad <hackingyseguridad@hackingyseguridad.com>
2018-10-22 21:50:38 +0200

    Update curl.sh

--- curl.sh
+++ curl.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
-# Ejemplo de peticion http con curl bien formada
-curl --insecure "http://192.168.1.254/index.html?" -H 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/38.0.2125.101 Safari/537.36' -H "Accept: text/html, applicattion/xhtml+xml, application/xml;q=0.9,*/*;q=0.8" -H 'Accept-Language: es-ES,es:q=0.5'
+curl --insecure --silent \
+-H "Accept: */*" \
+-H "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/38.0.2125.101 Safari/537.36" \
+-H "Accept: text/html, applicattion/xhtml+xml, application/xml;q=0.9,*/*;q=0.8" \
+-H "Accept-Language: es-ES,es;q=0.8,en-US;q=0.5,en;q=0.3" \
+-H "Accept-Encoding: gzip, deflate, br" \
+-H "Connection: keep-alive" \
+-H "Upgrade-Insecure-Requests: 1" \
+"http://192.168.1.254/index.html?"
