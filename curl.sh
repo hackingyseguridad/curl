efcd7749b94d65e587f4fa3c5a964a98ef14f9f5
hackingyseguridad <antonio_taboada@hotmail.es>
2018-10-30 22:16:33 +0100

    Update curl.sh

--- curl.sh
+++ curl.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 #
 # Ejemplo de peticion web completa y bien formada con el comendo Curl
-# Petición http://192.168.1.254/index.html?
+# Peticion http://192.168.1.254/index.html?
 # Credenciales user=admin passwd=12345 codificadas en Base64
 # www.hackingyseguridad.com
 #
+
 curl --insecure --silent --compressed \
 -H "User-Agent: Mozilla/5.0 (Windows NT 6.1; Win64; x64; rv:62.0) Gecko/20100101 Firefox/62.0" \
 -H "Accept: text/html, applicattion/xhtml+xml, application/xml;q=0.9,*/*;q=0.8" \
