c60d385165137b36f9d5171f57966a45ff52a10a
hackingyseguridad <antonio_taboada@hotmail.es>
2019-08-19 09:50:01 +0200

    Update curlapi.sh

--- curlapi.sh
+++ curlapi.sh
@@ -11,14 +11,13 @@
 #
 #
 #!/bin/bash
-echo "Uso.: ./urlssl.sh https://url"
-curl --cacert MyRootCA.crt -0 -3 -k -s -vvv \
+echo "Uso.: ./urlapi.sh https://url"
+curl --cacert MyRootCA.crt -vvv \
 -H "User-Agent: Mozilla/5.0 (Windows NT 6.1; Win64; x64; rv:62.0) Gecko/20100101 Firefox/62.0" \
 -H "Accept: text/html, applicattion/xhtml+xml, application/xml;q=0.9,*/*;q=0.8" \
 -H "Accept-Language: es-ES,es;q=0.8,en-US;q=0.5,en;q=0.3" \
 -H "Accept-Encoding: gzip, deflate, br" \
 -H "Connection: keep-alive" \
--H "Upgrade-Insecure-Requests: 1" \
 -H “Authorization: Bearer <JWT TOKEN>” \
 -H “Impersonate-Group: system:masters” \
 -H “Impersonate-User: null” \
