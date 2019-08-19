90e08461bba0bcc49346aea19192ac5d56145940
hackingyseguridad <antonio_taboada@hotmail.es>
2019-08-19 09:02:30 +0200

    Create curlapi.sh

---
+++ curlapi.sh
@@ -0,0 +1,26 @@
+#
+# Ver URL https SSL con curl
+# https://linux.die.net/man/1/curl
+# curl
+# -k fuerza a SSL en modo inseguro
+# -s --silent muestra en pantalla los datos solicitados
+# -vvv Mostrar detalle en el resultado
+# -3 sslv3
+# -0 http 1.0
+# curl --cacert my-ca.crt https://<ip>:<port>/api/v1/
+#
+#
+#!/bin/bash
+echo "Uso.: ./urlssl.sh https://url"
+curl --cacert MyRootCA.crt -0 -3 -k -s -vvv \
+-H "User-Agent: Mozilla/5.0 (Windows NT 6.1; Win64; x64; rv:62.0) Gecko/20100101 Firefox/62.0" \
+-H "Accept: text/html, applicattion/xhtml+xml, application/xml;q=0.9,*/*;q=0.8" \
+-H "Accept-Language: es-ES,es;q=0.8,en-US;q=0.5,en;q=0.3" \
+-H "Accept-Encoding: gzip, deflate, br" \
+-H "Connection: keep-alive" \
+-H "Upgrade-Insecure-Requests: 1" \
+-H “Authorization: Bearer <JWT TOKEN>” \
+-H “Impersonate-Group: system:masters” \
+-H “Impersonate-User: null” \
+-H “Accept: application/json” \ 
+"$1"
