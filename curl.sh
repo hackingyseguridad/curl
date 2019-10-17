2ec3b6d587cee6bee79149cf1888835cfffc2a36
hackingyseguridad <antonio_taboada@hotmail.es>
2019-10-17 09:59:34 +0200

    Update curl.sh

--- curl.sh
+++ curl.sh
@@ -12,9 +12,8 @@
 #
 #!/bin/bash
 echo "Uso.: ./curl.sh https://url"
-curl -k -vvv \
+curl -k -vvvv $1 \
 -H "User-Agent: Mozilla/5.0 (Windows NT 6.1; Win64; x64; rv:62.0) Gecko/20100101 Firefox/62.0" \
 -H "Accept: text/html, applicattion/xhtml+xml, application/xml;q=0.9,*/*;q=0.8" \
 -H "Accept-Language: es-ES,es;q=0.8,en-US;q=0.5,en;q=0.3" \
 -H "Connection: keep-alive" \
-"$1"
