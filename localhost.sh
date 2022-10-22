754d1d3e7048d01866544a2a8662137e6a80eaa6
hackingyseguridad <antonio_taboada@hotmail.es>
2022-10-22 23:08:12 +0200

    Create localhost.sh

---
+++ localhost.sh
@@ -0,0 +1,1 @@
+curl -vvv -s -k -X 'GET' -H "Host: 127.0.0.1" -H "User-Agent: Mozilla/5.0 (Windows NT 6.1; Win64; x64; rv:62.0) Gecko/20100101 Firefox/62.0" -H "Accept: text/html, applicattion/xhtml+xml, application/xml;q=0.9,*/*;q=0.8"  -H "Accept-Language: es-ES,es;q=0.8,en-US;q=0.5,en;q=0.3" $1
